8d6ec0ec614de5b8f2b7fff8f6e0a046ab8f6a85
Ubuntu <ubuntu@ip-172-31-43-170.ap-south-1.compute.internal>
2023-12-19 19:15:27 +0000

    cses questions

---
+++ cses.cpp
@@ -0,0 +1,15 @@
+// https://cses.fi/problemset/task/1068
+
+#include <iostream>
+using namespace std;
+
+void evaluate(int *n){
+    ((*n)%2==0) ? *n = (*n)/2 : ((*n)*3)+1
+
+}
+
+int main(){   
+    int n;
+    cin >> n; 
+     return 0;
+}
